abaa4bd72839b362d0d47b0f9e76772dadd87601
JerinBoyd <maxers@mac.com>
2018-02-11 21:07:58 -0500

    added reset button

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
          </ul>
          <input type="text" placeholde='Enter Todo' ref={(input) => this.todoInput = input} />
          <button onClick={this.addTodo.bind(this)}>Add</button>
+         <div><button type="reset">Reset List</button></div>
 
 
 
